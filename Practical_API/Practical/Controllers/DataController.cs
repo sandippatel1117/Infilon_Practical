7d259a36afc8bf62c3a8ded013a8f2c5e08b563b
DESKTOP-3JNKFFV\sandi <sandippatel1117@gmail.com>
2022-08-01 08:00:11 +0530

    Project implementation

---
+++ Practical_API/Practical/Controllers/DataController.cs
@@ -0,0 +1,91 @@
+﻿using Practical.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace Practical.Controllers
+{
+    public class DataController : ApiController
+    {
+        public IHttpActionResult GetUsers()
+        {
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+                    //HTTP GET
+                    var responseTask = client.GetAsync("todos");
+                    responseTask.Wait();
+
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+
+                        var readTask = result.Content.ReadAsAsync<List<User>>();
+                        readTask.Wait();
+
+                        var users = readTask.Result;
+
+                        DataTable dt = ToDataTable(users);
+
+
+
+                        using (SqlConnection con = new SqlConnection(constr))
+                        {
+                            using (SqlCommand cmd = new SqlCommand("Update_Users_API"))
+                            {
+                                cmd.CommandType = CommandType.StoredProcedure;
+                                cmd.Connection = con;
+                                cmd.Parameters.AddWithValue("@tblUsers", dt);
+                                con.Open();
+                                cmd.ExecuteNonQuery();
+                                con.Close();
+                            }
+                        }
+
+                    }
+                }
+
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+        public DataTable ToDataTable<T>(List<T> items)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+            //Get all the properties
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in Props)
+            {
+                //Setting column names as Property names
+                dataTable.Columns.Add(prop.Name);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    //inserting property values to datatable rows
+                    values[i] = Props[i].GetValue(item, null);
+                }
+                dataTable.Rows.Add(values);
+            }
+            //put a breakpoint here and check datatable
+            return dataTable;
+        }
+    }
+}
